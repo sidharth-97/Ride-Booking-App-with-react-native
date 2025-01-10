ea6a300edc3c1c6851873471bae272538f32b1e3
Sidharth PT <sidharthanand73@gmail.com>
2025-01-10 07:21:28 +0530

    feat: payment with stripe

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,60 +1,15 @@
 // @ts-nocheck
 import { icons } from "@/constants";
+import { useFetch } from "@/lib/fetch";
 import { calculateRegion, generateMarkersFromData } from "@/lib/map";
 import { useDriverStore, useLocationStore } from "@/store";
 import { MarkerData } from "@/types/type";
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 
-const drivers = [
-  {
-    id: 1,
-    first_name: "James",
-    last_name: "Wilson",
-    profile_image_url:
-      "https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/",
-    car_image_url:
-      "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
-    car_seats: 4,
-    rating: 4.8,
-  },
-  {
-    id: 2,
-    first_name: "David",
-    last_name: "Brown",
-    profile_image_url:
-      "https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/",
-    car_image_url:
-      "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
-    car_seats: 5,
-    rating: 4.6,
-  },
-  {
-    id: 3,
-    first_name: "Michael",
-    last_name: "Johnson",
-    profile_image_url:
-      "https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/",
-    car_image_url:
-      "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
-    car_seats: 4,
-    rating: 4.7,
-  },
-  {
-    id: 4,
-    first_name: "Robert",
-    last_name: "Green",
-    profile_image_url:
-      "https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/",
-    car_image_url:
-      "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
-    car_seats: 4,
-    rating: 4.9,
-  },
-];
-
 const Map = () => {
+  const { data: drivers, loading, error } = useFetch("/(api)/driver");
   const {
     userLatitude,
     userLongitude,
@@ -70,8 +25,6 @@ const Map = () => {
     destinationLongitude,
   });
   useEffect(() => {
-    setDrivers(drivers);
-
     if (Array.isArray(drivers)) {
       if (!userLatitude || !userLongitude) return;
 
@@ -83,6 +36,43 @@ const Map = () => {
       setMarkers(newMarkers);
     }
   }, [drivers, userLatitude, userLongitude]);
+
+  useEffect(() => {
+    if (markers.length > 0 && destinationLatitude && destinationLongitude) {
+      calculateDriverTimes({
+        markers,
+        userLatitude,
+        userLongitude,
+        destinationLatitude,
+        destinationLongitude,
+      }).then((data) => {
+        setDriverTimes(data as MarkerData[]);
+      });
+    }
+  }, [
+    markers,
+    destinationLatitude,
+    destinationLongitude,
+    userLatitude,
+    userLongitude,
+  ]);
+
+  if (loading || !userLatitude || !userLongitude) {
+    return (
+      <View className="flex justify-between items-center w-full">
+        <ActivityIndicator size="small" color="#000" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className="flex justify-between items-center w-full">
+        <Text>Error: {error}</Text>
+      </View>
+    );
+  }
+
   return (
     <View className="w-full h-full rounded-2xl">
       <MapView
